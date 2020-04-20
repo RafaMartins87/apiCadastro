71692b34b8e7d0aba77705a48c95b6d146586aaa
Rafael <rafael.melo@modalgr.com.br>
2020-04-20 11:30:04 -0300

    add model class

---
+++ Api/Models/CadastroModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class CadastroModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+
+    }
+}
